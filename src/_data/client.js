ce90feee3e9bba3b46574c96a944c2b199a3f0b3
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-05-05 17:08:38 +0100

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Siam Thai Spa",
+    "email": "siamthaispacricklewood@gmail.com",
+    "phoneForTel": "+442084522690",
+    "phoneFormatted": "020 8452 2690",
+    "openingHours": "Mon-Sun: 11am-10pm",
+    "onlineBooking": "https://siamthaispa.setmore.com/",
+    "address": {
+        "lineOne": "85 Cricklewood Broadway",
+        "lineTwo": "",
+        "city": "London ",
+        "state": "",
+        "zip": "NW2 3JG",
+        "country": "UK",
+        "mapLink": "https://maps.app.goo.gl/jyWHrBTgUTo8nPyY8"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+        "googleReview": "https://g.page/r/Cbpf3E0ziVD4EBM/review",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://siamthaispacricklewood.co.uk"
+};
